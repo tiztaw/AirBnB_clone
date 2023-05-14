d0bd690a985835997e68097cc6d1481863d7d16b
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-05-14 20:54:41 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""
-0x00. AirBnB clone - The console
-Review module
-"""
+"""Module for Review class."""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """
-    Defines attributes/methods for the Review class, subclass of BaseModel
-    Other attributes/methods are inherited from BaseModel
-    """
-
+    """Class representing a Review."""
     place_id = ""
     user_id = ""
     text = ""
-
-    # def __init__(self, *args, **kwargs):
-    #     """initialize variables and methods"""
-    #     super().__init__(self, *args, **kwargs)
