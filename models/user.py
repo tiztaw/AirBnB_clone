3e9d07a9f6098c576777eedfcdf92419d032f29a
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-05-14 20:55:41 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""
-0x00. AirBnB clone - The console
-User module
-"""
+"""Module for User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    Defines attributes/methods for the User class, subclass of BaseModel
-    Other attributes/methods are inherited from BaseModel
-    """
-
+    """Class representing a User."""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    # def __init__(self, *args, **kwargs):
-    #     """initialize variables and methods"""
-    #     super().__init__(self, *args, **kwargs)
