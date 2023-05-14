ff1bb0b23da2415bcb71854e2145b26bae9aeb89
tiztaw <49856616+tiztaw@users.noreply.github.com>
2023-05-14 20:53:43 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-"""
-0x00. AirBnB clone - The console
-City module
-"""
+"""Module for City class."""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    Defines attributes/methods for the City class, subclass of BaseModel
-    Other attributes/methods are inherited from BaseModel
-    """
-
+    """Class representing a City."""
     state_id = ""
     name = ""
-
-    # def __init__(self, *args, **kwargs):
-    #     """initialize variables and methods"""
-    #     super().__init__(self, *args, **kwargs)
